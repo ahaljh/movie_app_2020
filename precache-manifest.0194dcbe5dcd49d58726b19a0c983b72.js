9cccec6bfc0702228f3ce53529b359812d1d4ae2
ahaljh <ahaljh@gmail.com>
2020-06-08 18:53:46 +0900

    Updates

---
+++ precache-manifest.0194dcbe5dcd49d58726b19a0c983b72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b50a7ad5be9fd0d49d0893ab190b484",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "b7d1743cfcf37be8a0e0",
+    "url": "/movie_app_2020/static/css/main.61dda368.chunk.css"
+  },
+  {
+    "revision": "b17fb38856ffb36d44ef",
+    "url": "/movie_app_2020/static/js/2.14c54020.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.14c54020.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7d1743cfcf37be8a0e0",
+    "url": "/movie_app_2020/static/js/main.e4b0b3b5.chunk.js"
+  },
+  {
+    "revision": "86e4940ccaeed0f37e85",
+    "url": "/movie_app_2020/static/js/runtime-main.83f6590a.js"
+  }
+]);

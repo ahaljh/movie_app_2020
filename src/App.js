8c4d2c106d5d26d7128cbd755cdb5956440623d9
ahaljh <ahaljh@gmail.com>
2020-06-08 14:10:03 +0900

    Chapter 3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
 
+/*
+function Food(props) {
+  ({fav} = props); 
+
+  return <h3>I like {fav}.</h3>; 
+}
+*/
+
+function Food({fav}) {
+  return <h3>I like {fav}</h3>; 
+}
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Food fav="kimchi" />
+      <Food fav="potato" />
+      <Food fav="ramen" />
+    </div>
+  ); 
 }
 
 export default App;

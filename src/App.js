5a324589a18b01deb7a82a1a515e5211813766be
ahaljh <ahaljh@gmail.com>
2020-06-08 14:33:32 +0900

    Chapter 4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types'; 
 
 /*
 function Food(props) {
@@ -8,19 +9,48 @@ function Food(props) {
 }
 */
 
-function Food({fav}) {
-  return <h3>I like {fav}</h3>; 
+function Food({name, picture}) {
+  return (
+    <div>
+      <h2>I like {name} </h2>
+      <img src={picture} alt={name}/>
+    </div>
+  );
 }
+
+const foodLike = [
+  {
+    id : 1,
+    name : 'kimchi' ,
+    image : 'https://www.bgw.kr/wp-content/uploads/2019/12/%EC%88%98%EC%9E%85%EA%B9%80%EC%B9%98.png', 
+    rating : 4.4
+  },
+  {
+    id : 2,
+    name : "potato", 
+    image : "https://m.eejmall.com/web/product/big/201708/211_shop1_627935.jpg", 
+    rating : 3.0 
+  }, 
+  {
+    id : 3, 
+    name : "ramen", 
+    image : "https://img.hankyung.com/photo/201902/AA.19048482.1.jpg", 
+    rating : 4.0 
+  }
+] ; 
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Food fav="kimchi" />
-      <Food fav="potato" />
-      <Food fav="ramen" />
+      {foodLike.map(dish => (<Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />))}
     </div>
   ); 
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired, 
+  picture: PropTypes.string.isRequired, 
+  rating: PropTypes.number ,
 }
 
 export default App;
